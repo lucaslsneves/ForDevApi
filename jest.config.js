1c981504b4eb8f4bf7e6d5390646ccd707e28890
Lucas Luz <lucasluzthatsme@gmail.com>
2021-01-06 08:02:55 -0300

    chore: test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   preset: 'ts-jest'
-};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import express from 'express'
-
-const app = express()
-
-app.get('/', (req, res) => res.json({ message: 'hello world' }))
-
-app.listen(3333)
+console.log('oi')
