36b99360ebe2321cc0a005b1b7f0f90ccf9555da
Lucas Luz <lucasluzthatsme@gmail.com>
2021-01-28 10:48:56 -0300

    test: ensure LogControllerDecorator returns the same result of controller

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -6,7 +6,9 @@ const makeController = (): Controller => {
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
       const httpResponse:HttpResponse = {
         statusCode: 200,
-        body: httpRequest.body
+        body: {
+          name: 'Lucas'
+        }
       }
       return new Promise(resolve => resolve(httpResponse))
     }
@@ -41,5 +43,25 @@ describe('LogControllerDecorator', () => {
     }
     sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  it('should return the same result of controller handle method ', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest : HttpRequest = {
+      body: {
+        email: 'any@email.com,',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Lucas'
+      }
+    })
   })
 })
