142a8aef79deabe9c1335738e61205d6652bc78d
Lucas Luz <lucasluzthatsme@gmail.com>
2021-01-28 10:59:22 -0300

    refactor: make server error return stack

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,7 +4,7 @@ import { AccountMongoRepository } from '../../infra/db/mongodb/account-repositor
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
-import { LogControllerDecorator } from '../decorators/log'
+import { LogControllerDecorator } from '../decorators/log-controller-decorator'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
