83ffbd5c024a7eb71fa05eb02dbd9807623e5acc
Lucas Luz <lucasluzthatsme@gmail.com>
2021-01-29 19:53:44 -0300

    test: ensure ValidationComposite return null on success

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
@@ -7,24 +7,32 @@ class ValidationStub implements Validation {
     return null
   }
 }
-
+const makeValidationsStubs = (): Validation[] => [
+  new ValidationStub(),
+  new ValidationStub()
+]
 describe('ValidationComposite', () => {
   it('Should return the same Validations error', () => {
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([
-      validationStub
-    ])
-    jest.spyOn(validationStub, 'validate').mockReturnValue(new MissingParamError('field'))
+    const validationStubs = makeValidationsStubs()
+    const sut = new ValidationComposite(validationStubs)
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
-  it('Should return null if validation success', () => {
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([
-      validationStub
-    ])
+  it('Should return the first Validations error found it', () => {
+    const validationStubs = makeValidationsStubs()
+    const sut = new ValidationComposite(validationStubs)
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new InvalidParamError('field'))
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new InvalidParamError('field'))
     const error = sut.validate({ field: 'any_value' })
-    expect(error).toEqual(null)
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+
+  it('Should return null if validation success', () => {
+    const validationStubs = makeValidationsStubs()
+    const sut = new ValidationComposite(validationStubs)
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
   })
 })
